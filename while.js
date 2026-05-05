9ee11a2a0b54dffaf99c066b45081cc66f8594bd
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-05 10:17:27 -0400

    while loop

---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if(number < 10){
+    console.log('Choto ekta number paisi');
+    
+}
+console.log("Something new is cooking");
+
+// --------------- WHILE LOOP ---------------
+let num = 0;
+while(num < 5){
+    console.log('loooooooping', num);
+    // num += 1;
+    num++;
+}
