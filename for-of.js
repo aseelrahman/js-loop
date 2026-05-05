936ea74039e8e891bf6a1e60c8109d5b3e78f449
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-05 09:59:38 -0400

    loop in js

---
+++ for-of.js
@@ -0,0 +1,18 @@
+const numbers = [12, 45, 78, 56, 90, 10]
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+    
+}
+
+
+const fruits = ['orange', 'apple', 'banana', 'watermelon']
+
+
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I wanna eat.');
+    
+}
